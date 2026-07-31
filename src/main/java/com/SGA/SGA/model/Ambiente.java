df180844ce67babb2015eaff36c75f9082fc4fb1
Ny <168129793+Nycolas64@users.noreply.github.com>
2026-07-31 14:20:42 -0300

    Nome definido como obrigatório na entidade Ambiente

--- src/main/java/com/SGA/SGA/model/Ambiente.java
+++ src/main/java/com/SGA/SGA/model/Ambiente.java
@@ -1,76 +1,45 @@
 package com.SGA.SGA.model;
 
-
-
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Column;
 
-    @Entity
-    public class Ambiente {
+@Entity
+public class Ambiente {
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-        private String nome;
+    @Column(nullable = false)
+    private String nome;
 
-        private String descricao;
+    private String descricao;
 
-        private Integer capacidade;
+    private Integer capacidade;
 
-        private String equipamentos;
+    private String equipamentos;
 
-        public Ambiente() {
-        }
-
-        public Ambiente(String nome, String descricao, Integer capacidade, String equipamentos) {
-            this.nome = nome;
-            this.descricao = descricao;
-            this.capacidade = capacidade;
-            this.equipamentos = equipamentos;
-        }
-
-        public Long getId() {
-            return id;
-        }
-
-        public void setId(Long id) {
-            this.id = id;
-        }
-
-        public String getNome() {
-            return nome;
-        }
-
-        public void setNome(String nome) {
-            this.nome = nome;
-        }
-
-        public String getDescricao() {
-            return descricao;
-        }
-
-        public void setDescricao(String descricao) {
-            this.descricao = descricao;
-        }
-
-        public Integer getCapacidade() {
-            return capacidade;
-        }
-
-        public void setCapacidade(Integer capacidade) {
-            this.capacidade = capacidade;
-        }
-
-        public String getEquipamentos() {
-            return equipamentos;
-        }
-
-        public void setEquipamentos(String equipamentos) {
-            this.equipamentos = equipamentos;
-        }
+    public Ambiente() {
     }
 
+    public Ambiente(String nome, String descricao, Integer capacidade, String equipamentos) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.capacidade = capacidade;
+        this.equipamentos = equipamentos;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public String getDescricao() { return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+    public Integer getCapacidade() { return capacidade; }
+    public void setCapacidade(Integer capacidade) { this.capacidade = capacidade; }
+    public String getEquipamentos() { return equipamentos; }
+    public void setEquipamentos(String equipamentos) { this.equipamentos = equipamentos; }
+}
